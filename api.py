b8d35440a39af0ce38e1e462aa31b84362b9a5c5
tsetty <tsetty@outbrain.com>
2021-11-28 21:51:45 +0200

    added env for url

--- api.py
+++ api.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_restful import Resource, Api
 import requests, json
 import random
-
+import os
 app = Flask(__name__)
 api = Api(app)
-
+url = os.getenv("DOGS_URL")
 class GetDogFact(Resource):
     def get(self):
         fact_list = requests.get("http://dogfacts:5000/api/v1/resources/dogs/all").json()
