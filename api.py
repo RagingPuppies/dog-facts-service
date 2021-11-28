698c1719c6aa0ec17d8729e237817d2a3679efc4
tsetty <tsetty@outbrain.com>
2021-11-28 21:49:06 +0200

    added code

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import requests, json
+import random
+
+app = Flask(__name__)
+api = Api(app)
+
+class GetDogFact(Resource):
+    def get(self):
+        fact_list = requests.get("http://dogfacts:5000/api/v1/resources/dogs/all").json()
+        random_fact = random.choice(fact_list)['fact']
+        return {'fact': str(random_fact)}
+
+api.add_resource(GetDogFact, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
